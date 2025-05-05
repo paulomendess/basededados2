a56eb0e8c386121f20fdb01b158796224bfcc883
Paulo Mendes <itzfs233@gmail.com>
2025-05-05 23:29:23 +0100

    1

---
+++ basededados2-main/api/index.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/about')
+def about():
+    return 'About'
+
+@app.route('/top')
+def top():
+    return 'se der o paulo lambe me a gaita'
